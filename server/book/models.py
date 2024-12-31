67def8023fff75a21790ba864a8bf7658bf4e568
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-12-31 11:55:12 +0500

    logs for checking python, model instance and json conversion

--- server/book/models.py
+++ server/book/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Book (models.Model):
+    title= models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    language = models.CharField(max_length=30, default='English')
+
+    # def __str__(self):
+    #     return self.title
+
--- server/book/views.py
+++ server/book/views.py
@@ -1,17 +1,51 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Book
+# import json
 
-class CreateBook (APIView):
-    def get(self, request):
+class CreateBook(APIView):
+    def post(self, request):
+        # log raw request.body
+        print("RAW",request.body)
+        # logs the json data coverted into python object
+        data = request.data
+        print("Requested data",request.data)
+        title = data.get("title")
+        author = data.get("author")
+        language = data.get("language", "English") 
+
+        if not title or not author:
+            return Response({
+                "status": 400,
+                "message": "Title or author is missing"
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        book = Book.objects.create(title=title, author=author, language=language)
+        print("Book created:", book)
         return Response({
             "status": 201,
-            "message": "book created successfully",
-            "data": "_id: 123456"
-        })
+            "message": "Book created successfully",
+            "data": {
+                "id": book.id,
+                "title": book.title,
+                "author": book.author,
+                "language": book.language
+            },
+        }, status=status.HTTP_201_CREATED)       
 
 class GetBook (APIView):
     def get (self, request):
-        return Response({
-            "status": "200",
-            "message": "book fetched successfully"
-        })
+        books = Book.objects.all()
+        print(books)
+        book_list = [
+            {
+                "id": book.id,
+                "title": book.title,
+                "author": book.author,
+                "language": book.language,
+            }
+            for book in books
+        ]
+        print(book_list)
+        return Response({"status": 200, "message": "All books fetched successfully", "data": book_list}, status=status.HTTP_200_OK)
